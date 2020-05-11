c6a8464776aa89f39c81e928f39c2ddc8435f20b
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-11 22:04:24 +0530

    blocking script

--- block.bat
+++ block.bat
@@ -3,7 +3,11 @@ cls
 title spotify adblock
 color 70
 echo Backing up the hosts
-copy %windir%\system32\drivers\etc\hosts 
-for /F "tokens=*" %%i in (adserverList.txt) do (
-set line=127.0.0.1 %%i
+rem hosts
+copy %windir%\system32\drivers\etc\hosts
+echo Hosts backed up!
+echo Writing into hosts
+for /f "tokens=*" %%i in (adserverList.txt) do (
+echo 127.0.0.1 %%i>>%windir%\system32\drivers\etc\hosts
+
 )
